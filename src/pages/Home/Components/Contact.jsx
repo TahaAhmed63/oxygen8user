a4ae9406fdca06c982395663f5b9f53cb2af3665
unknown <Meeting Room@husnaininter.hnh.local>
2023-05-15 19:54:28 +0500

    commit now

--- src/pages/Home/Components/Contact.jsx
+++ src/pages/Home/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./style.css"
+import "./Style.css"
 import { Container,Row,Col } from 'react-bootstrap'
 import {RxEnvelopeClosed} from 'react-icons/rx'
 import {BsInstagram} from 'react-icons/bs'
