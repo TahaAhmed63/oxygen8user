ce99a3b090360fb679a62d580ba29ab8f48e0918
tahaahmed12 <tahaahmed@hnhtechsolutions.com>
2023-05-29 21:30:15 +0500

    done all

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -12,19 +12,35 @@ function Card({...item}) {
 
   return (
     <div
-      className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 grid-item cat1 cat2 cat4"
+      className="col-xxl-12 col-xl-12 col-lg-4 col-md-6 grid-item cat1 cat2 cat4"
       onClick={() => {
         getDetail(id);
       }}
     >
+
       <div className="course__item white-bg mb-30 fix">
+      <div className="row">
+      <div className="col-xxl-4 col-xl-4 col-lg-4">
         <div className="course__thumb w-img p-relative fix">
           <Link>
             <img src={`${img_link}${image}`} alt="" width="300" height="250" />
           </Link>
         </div>
-        <div className="course__content">
-          <div className="course__meta d-flex align-items-center justify-content-between">
+        </div>
+        <div className="col-xxl-8 col-xl-8 col-lg-8">
+        <div className="course__content" style={{
+           
+            padding:" 2px 30px",
+            height: "100%",
+            display: "flex",
+gap:"11px",
+       flexDirection: "column",
+            justifyContent: "space-between"
+        
+        }}>
+          <div className="course__meta d-flex align-items-center justify-content-between" style={{
+            paddingTop:"22px"
+          }}>
             <div className="course__lesson">
               <span>
                 <i className="far fa-book-alt"></i>
@@ -33,12 +49,13 @@ function Card({...item}) {
             </div>
           </div>
           <h3
-            className="course__title text-start pt-5"
+            className="course__title text-start pt-5 " style={{
+              marginBottom:"5rem"
+            }}
           >
             <Link style={{textDecoration:"none",color:"#000"}}>{name}</Link>
           </h3>
-        </div>
-        <div className="course__more d-flex justify-content-between align-items-center">
+            <div className="course__more d-flex justify-content-between align-items-flex-end mb-5" style={{paddingLeft:"0px"}}>
           <div className="course__status">
             <span style={{ color: "#337c75" }}>{pack ? 'Enroll' : 'Paid'}</span>
           </div>
@@ -50,6 +67,23 @@ Find Out More
               <i className="far fa-arrow-right"></i>
             </Link>
           </div>
+          </div>
+        </div>
+        </div>
+
+        {/* <div className="course__more d-flex justify-content-between align-items-center">
+          <div className="course__status">
+            <span style={{ color: "#337c75" }}>{pack ? 'Enroll' : 'Paid'}</span>
+          </div>
+          <div className="course__btn">
+            <Link style={{textDecoration:"none"}} href="#" className="link-btn" to="/coursedetail">
+
+Find Out More
+              <i className="far fa-arrow-right"></i>
+              <i className="far fa-arrow-right"></i>
+            </Link>
+          </div>
+          </div> */}
         </div>
       </div>
     </div>
