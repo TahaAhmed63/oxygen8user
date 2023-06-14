e837b0b3de857872ea147afc991c0f3ba20a5a55
tahaahmed12 <tahaahmed@hnhtechsolutions.com>
2023-06-14 20:12:46 +0500

    done video

--- src/pages/Home/Components/DetailVideo.jsx
+++ src/pages/Home/Components/DetailVideo.jsx
@@ -1,78 +1,78 @@
-import React,{useState,useRef} from 'react'
-import "./style.css"
-import { Container,Row,Col } from 'react-bootstrap'
-import mainvideo from "./../../../../src/Assets/videohome/090523_Oxygen8-Website-Medium-Loop_4x3.mp4"
-// import skeleton from "./../../../../src/Assets/Videoaboutimages/skeleton.png"
-import playButtonIcon from "./../../../Assets/AboutImages/videobtn-removebg-preview.png"
-import mylogo from "./../../../Assets/AboutImages/Oxygen icon W.png"
+import React, { useState, useRef, useEffect } from 'react';
+import './style.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import mainvideo from './../../../../src/Assets/videohome/090523_Oxygen8-Website-Medium-Loop_4x3.mp4';
+import playButtonIcon from './../../../Assets/AboutImages/videobtn-removebg-preview.png';
+import mylogo from './../../../Assets/AboutImages/Oxygen icon W.png';
 
 export const DetailVideo = () => {
   const videoRef = useRef(null);
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [showPlayButton, setShowPlayButton] = useState(true);
 
   const handleVideoToggle = () => {
-    if (isVideoPlaying) {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+
+      setIsVideoPlaying(true);
+    } else {
       videoRef.current.pause();
       setIsVideoPlaying(false);
-    } else {
-      videoRef.current.play();
-      setIsVideoPlaying(true);
     }
   };
+  
+  useEffect(() => {
+    if(videoRef.current){
+      // videoRef.current.autoplay = true
+      console.log(videoRef)
+      
+    }
+    const handleCanPlay = () => {
+      setShowPlayButton(false);
+    };
 
+    videoRef.current.addEventListener('canplay', handleCanPlay);
+
+    return () => {
+      videoRef.current.removeEventListener('canplay', handleCanPlay);
+    };
+  }, []);
 
   return (
-<>
-
-<section className='Home-detail'>
-<div className="watermark-container">
-<div className="bottommark-2"></div> 
-</div>
-
-
-<Container fluid>
-
-  <Container>
-    <Row className='flex-direction-column'>
-    
-       <Col md={12} xl={12} xxl={12} className='video-details'>
-        <div className="head-wrap">
-            <h3 className='text-center'>Conscious Nervous System Feedback </h3>
-            <h3 className='text-center'>(CNSF) and Optimisation</h3>
+    <>
+      <section className="Home-detail">
+        <div className="watermark-container">
+          <div className="bottommark-2"></div>
         </div>
-        <div className='middle-img'>
-          <img src={mylogo}  alt="" />
-        </div>
-        <div className="video-container-2">
-      <video ref={videoRef}controls playsInline>
-              <source src={mainvideo} type="video/mp4" />
-              <source src={mainvideo} type="video/webm" />
-              Your browser does not support the video tag.
-            </video>           
-                   {!isVideoPlaying && (
+
+        <Container fluid>
+          <Container>
+            <Row className="flex-direction-column">
+              <Col md={12} xl={12} xxl={12} className="video-details">
+                <div className="head-wrap">
+                  <h3 className="text-center">Conscious Nervous System Feedback</h3>
+                  <h3 className="text-center">(CNSF) and Optimisation</h3>
+                </div>
+                <div className="middle-img">
+                  <img src={mylogo} alt="" />
+                </div>
+                <div className="video-container-2">
+                  <video ref={videoRef}  playsInline autoPlay controls >
+                    <source src={mainvideo} type="video/mp4"  />
+                    <source src={mainvideo} type="video/webm" />
+                    Your browser does not support the video tag.
+                  </video> 
+                  { !isVideoPlaying && (
                     <div className="play-button" onClick={handleVideoToggle}>
                       <img src={playButtonIcon} alt="Play Button" />
                     </div>
                   )}
-            
                 </div>
-       </Col>
-     {/* <Col md={4} xl={4} xxl={4} className='img-section'>
-      
-   
-        <div className="about-img-wrap">
-          <img src={skeleton} alt="" className='img-fluid' width='300' height='900'/>
-        </div>
-     
-     
-     </Col> */}
-    </Row>
-</Container>
-</Container>
-
-</section>
-
-
-</>
-  )
-}
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      </section>
+    </>
+  );
+};
