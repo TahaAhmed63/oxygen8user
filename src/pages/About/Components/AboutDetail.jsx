a41932e0481e33992210f5cbf7334dbc2e6f84da
tahaahmed12 <tahaahmed@hnhtechsolutions.com>
2023-06-27 15:31:26 +0500

    all done

--- src/pages/About/Components/AboutDetail.jsx
+++ src/pages/About/Components/AboutDetail.jsx
@@ -1,58 +1,49 @@
-import React from 'react'
-import "./style.css"
-import { Row,Col } from 'react-bootstrap'
+import React from "react";
+import "./style.css";
+import { Row, Col } from "react-bootstrap";
 
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap";
 // import about1 from './../../../../src/Assets/AboutImages/About1.jpg'
 // import about2 from "./../../../../src/Assets/AboutImages/About2.jpg"
 // import about3 from "./../../../../src/Assets/AboutImages/About3.jpg"
 // import about4 from "./../../../../src/Assets/AboutImages/About4.jpg"
 
-
-
-
 export const AboutDetail = () => {
-
-// const myimage=[
-//     {
-//         myimg:about1
-//     },
-//     {
-//         myimg:about2
-//     },
-//     {
-//         myimg:about3
-//     },
-//     {
-//         myimg:about4
-//     },
-// ]
-
+  // const myimage=[
+  //     {
+  //         myimg:about1
+  //     },
+  //     {
+  //         myimg:about2
+  //     },
+  //     {
+  //         myimg:about3
+  //     },
+  //     {
+  //         myimg:about4
+  //     },
+  // ]
 
   return (
     <>
- <section className="AboutPage">
+      <section className="AboutPage">
         <Row>
-     
-    <Col md={12} sm={12}>
-        <div className="headwrapabout">
-           <h1>About</h1>
-        </div>
-    </Col>
-
-
+          <Col md={12} sm={12}>
+            <div className="headwrapabout">
+              <h1>About</h1>
+            </div>
+          </Col>
         </Row>
-
-    </section> 
-    <section className='corperate-head'>
+      </section>
+      <section className="corperate-head">
         <Container>
-           <div className='corp-head-wrap'>
-            <div className='corphead'>
+          <div className="corp-head-wrap">
+            <div className="corphead">
               <h3>
                 "Overnight I went from building a spacecraft for NASA to not
-being able to leave my bed...as part of the recovery process I 
-developed an effective toolkit of stress solutions that I truly believe ~
-can benefit anyone."
+                being able to leave my bed...as part of the recovery process I
+                developed an effective toolkit of stress solutions that I truly
+                believe can benefit anyone."
               </h3>
             </div>
             {/* <div className="para-corp">
@@ -60,13 +51,10 @@ can benefit anyone."
                  -Center of Ecnomic and Bussiness Research
               </p>
             </div> */}
-           </div>
+          </div>
         </Container>
-        
-
-
-         </section>
-{/* <section className="About-image py-5">
+      </section>
+      {/* <section className="About-image py-5">
         <Container fluid>
          <Container>
              <Row className='close-img'>
@@ -104,6 +92,5 @@ can benefit anyone."
     
     </section>  */}
     </>
-    
-  )
-}
+  );
+};
